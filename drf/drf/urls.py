fe8be4624fd3caaaf97e64c839bc67b09304598e
Touhou09 <touhou09@gmail.com>
2023-08-14 03:00:27 +0000

    debug setting

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
-]
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+
+] 
+
 
 """ 
 from rest_framework import permissions
