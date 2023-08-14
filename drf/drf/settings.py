cfb6e4e33dfb25df9ee615be3f7400ef05a620a7
Touhou09 <touhou09@gmail.com>
2023-08-14 01:42:47 +0000

    no guni and need csrf

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = 'django-insecure-e*!l*ixe_bu*(1v*jd!p#4e*6ak)%11__6aeanf3-kf+0ufk&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["main-platform-fswhtfifva-du.a.run.app", "localhost"]
+ALLOWED_HOSTS = ['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -35,14 +38,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ROOT_URLCONF = 'drf.urls'
 
@@ -118,11 +121,4 @@ MEDIA_ROOT = os.path.join('BASE_DIR', 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = [
-	"https://main-platform-fswhtfifva-du.a.run.app",
-    'http://localhost:4173',
-]
-
-
 
