c9c3e3a08644e82444a87c53ccfa7327f23b58b9
Touhou09 <touhou09@gmail.com>
2023-08-19 02:11:01 +0900

    unquteo

--- drf/posts/views.py
+++ drf/posts/views.py
@@ -8,8 +8,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
-import os
-import uuid
+from urllib.parse import unquote
 
 
 from rest_framework.exceptions import PermissionDenied
@@ -59,7 +58,7 @@ class PostListView(generics.ListCreateAPIView):
             # 파일 정보 저장
             for file_data in files_data:
                 
-                original_filename = file_data.name.encode('utf-8')  # Encode and then decode to ensure utf-8
+                original_filename = unquote(file_data.name)  # Decode URL-encoded filename
 
                 file_instance = File(file=file_data, post=serializer.instance)
                 file_instance.file.name = original_filename  # Set the original filename to the file instance
