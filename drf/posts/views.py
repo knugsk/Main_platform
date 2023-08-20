fb5b36fa33836c39e5d8c36937114928d5570adb
Touhou09 <touhou09@gmail.com>
2023-08-20 18:26:44 +0900

    boto file test 2

--- drf/posts/views.py
+++ drf/posts/views.py
@@ -164,8 +164,7 @@ class FileDownloadView(APIView):
             s3_client.download_file(S3_BUCKET_NAME, filename, os.path.join(settings.MEDIA_ROOT, filename))
             return Response("File downloaded successfully.", status=status.HTTP_200_OK)
         except Exception as e:
-            pass
-            # return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 from rest_framework.parsers import FileUploadParser
 
