62d08dfa72b87fee1f93d97166191550d21ccede
Touhou09 <touhou09@gmail.com>
2023-08-20 17:01:14 +0900

    boto file test 2

--- drf/posts/urls.py
+++ drf/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CategoryPostListView, PostListView, PostDetailView, CommentCreateView, CommentReplyCreateView, FileRetrieveUpdateDestroyView, CommentUpdateView
-from .views import FileUploadView
+from .views import FileUploadView, FileDownloadView
 
 urlpatterns = [
     path('categories/<int:id>/posts', CategoryPostListView.as_view(), name='category-detail'),
@@ -10,6 +10,6 @@ urlpatterns = [
     path('comments/<int:parent_comment_id>/replies/', CommentReplyCreateView.as_view(), name='comment-reply-create'),
     path('comments/<int:pk>/', CommentUpdateView.as_view(), name='comment-update-delete'),
     path('files/<int:pk>/', FileRetrieveUpdateDestroyView.as_view(), name='file-download'),
-    path('file/<str:filename>/', FileRetrieveUpdateDestroyView.as_view(), name='file-download'),
+    path('file/<str:filename>/', FileDownloadView.as_view(), name='file-download'),
     path('reupload/', FileUploadView.as_view(), name='file-upload'),
 ]
