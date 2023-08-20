53a1c2fc02198c20d31d1f5370856b1d0aa11dba
윤다민 <sctaemin@gmail.com>
2023-08-20 13:18:27 +0900

    배포 4치

--- src/pages/SubPage_Content.svelte
+++ src/pages/SubPage_Content.svelte
@@ -62,28 +62,31 @@
         window.open(link, '_blank');
     }
 
-    const downloadFile = async (link: string, filename: string) => {
-        var xhr = new XMLHttpRequest();
+    const downloadFile = (link) => {
+        const xhr = new XMLHttpRequest();
         xhr.open('GET', link, true);
         xhr.responseType = 'blob';
 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                var blob = xhr.response;
-                var blobUrl = window.URL.createObjectURL(blob);
+                const blob = xhr.response;
+                const blobUrl = window.URL.createObjectURL(blob);
 
-                var link = document.createElement('a');
-                link.href = blobUrl;
-                link.download = filename;
+                // 파일 이름 추출
+                const contentDisposition = xhr.getResponseHeader('content-disposition');
+                const filenameMatch = contentDisposition.match(/filename="(.+)"/);
+                const filename = filenameMatch ? filenameMatch[1] : 'downloaded_file';
 
-                var clickEvent = new MouseEvent('click', {
-                    view: window,
-                    bubbles: true,
-                    cancelable: false
-                });
+                // 파일 이름 인코딩
+                const encodedFilename = encodeURIComponent(filename);
 
-                link.dispatchEvent(clickEvent);
+                console.log(encodedFilename);
 
+                const a = document.createElement('a');
+                a.href = blobUrl;
+                a.download = encodedFilename; // 인코딩된 파일 이름 사용
+                const clickEvent = new MouseEvent('click', { view: window, bubbles: true, cancelable: false });
+                a.dispatchEvent(clickEvent);
                 window.URL.revokeObjectURL(blobUrl);
             }
         };
@@ -119,7 +122,7 @@
             <div class="board_files">
                 <p class="text_info_board_files">누르고 다운받으세요!</p>
                 {#each filterNonImageFiles(data.files) as file, index}
-                    <button class="box_file" on:click={() => {downloadFile(file.file, file.file);}}>
+                    <button class="box_file" on:click={() => {downloadFile(file.file);}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
                             <path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Z"></path>
                         </svg>
@@ -143,28 +146,28 @@
                     </button>
                 {/each}
                 {#each filterImages(data.files) as file, index}
-                <button class="box_file" on:click={() => {downloadFile(file.file, file.file);}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
-                        <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,16V158.75l-26.07-26.06a16,16,0,0,0-22.63,0l-20,20-44-44a16,16,0,0,0-22.62,0L40,149.37V56ZM40,172l52-52,80,80H40Zm176,28H194.63l-36-36,20-20L216,181.38V200ZM144,100a12,12,0,1,1,12,12A12,12,0,0,1,144,100Z"></path>
-                    </svg>
-                    <div class="box_file_des">
-                        <div class="card_file">
-                            <p class="text_name_file">{extractFileNameFromUrl(file.file)}</p>
-                        </div>
-                    </div>
-                    <button class="btn_delete_file" on:click|stopPropagation={async (event) => {
-                        event.stopPropagation();
-                        await delete_file(file.id).then((b) => {
-                            if(b) {
-                                get_data(params.content_id);
-                            }
-                        });
-                    }}>
-                        <svg class="svg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
-                            <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path>
-                        </svg>
-                    </button>
-                </button>
+                    <button class="box_file" on:click={() => {downloadFile(file.file);}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                          <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,16V158.75l-26.07-26.06a16,16,0,0,0-22.63,0l-20,20-44-44a16,16,0,0,0-22.62,0L40,149.37V56ZM40,172l52-52,80,80H40Zm176,28H194.63l-36-36,20-20L216,181.38V200ZM144,100a12,12,0,1,1,12,12A12,12,0,0,1,144,100Z"></path>
+                      </svg>
+                      <div class="box_file_des">
+                          <div class="card_file">
+                              <p class="text_name_file">{extractFileNameFromUrl(file.file)}</p>
+                          </div>
+                      </div>
+                      <button class="btn_delete_file" on:click|stopPropagation={async (event) => {
+                          event.stopPropagation();
+                          await delete_file(file.id).then((b) => {
+                              if(b) {
+                                  get_data(params.content_id);
+                              }
+                          });
+                      }}>
+                          <svg class="svg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+                              <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path>
+                          </svg>
+                      </button>
+                  </button>
                 {/each}
                 <button class="box_file center" on:click={async () => {await uploadFiles();}}>
                     <h1>+</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
     server: {
         strictPort: true,
         open: true,
+        https: true,
     },
     envPrefix: ["VITE_"],
-    plugins: [svelte(), tsconfigPaths()],
+    plugins: [svelte(), tsconfigPaths(), mkcert()],
 });
