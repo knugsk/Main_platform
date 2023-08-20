58e1ac5c37f03277ccbc7c0b1e466f522b866120
Touhou09 <touhou09@gmail.com>
2023-08-20 17:34:24 +0900

    boto file test 2

--- drf/drf/settings/aws.py
+++ drf/drf/settings/aws.py
@@ -4,6 +4,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 #  - "media" 폴더에 저장되도록 location 설정을 해줍니다.
 #  - "public-read" 권한으로 업로드되도록 default_acl 설정을 해줍니다.
 class AwsMediaStorage(S3Boto3Storage):
-    location='media'
+    location='media/post_files'
     default_acl='public-read'
     file_name_charset='utf-8'
