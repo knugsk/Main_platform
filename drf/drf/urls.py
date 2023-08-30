b02a60c7bb461955bc09d697141ad1e4fb43c81b
Touhou09 <touhou09@gmail.com>
2023-08-30 13:48:45 +0900

    db change

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -26,11 +26,11 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
 
 
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
 ] 
 
 
-
+""" 
 from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
@@ -52,3 +52,5 @@ urlpatterns += [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+ """
